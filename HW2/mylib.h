417d1c36548c1f22440c40c24aa1a27049edbc8f
Damian Parathyras <47918885+zdparathyras@users.noreply.github.com>
2023-05-03 19:23:33 +0200

    Delete HW2 directory

--- HW2/mylib.h
+++
@@ -1,9 +0,0 @@
-//my library
-#ifndef _mylib_h
-#define _mylib_h
-
-void ErrorFinder(int fd, int line);
-long int myread(int fd, char * result, long int max_bytes);
-long int mywrite (int fd, char str[],long int max_bytes);
-
-#endif
